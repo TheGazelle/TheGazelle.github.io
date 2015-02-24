6b4f7b07d54978d9075a34f1a12d0b76101c8e88
TheGazelle <grahamwhancock@gmail.com>
2015-02-24 13:05:55 -0500

    added grunt-jekyll

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,14 +30,38 @@ module.exports = function(grunt) {
       options: {
         port: 8001
       }
-    }
+    },
+    jekyll: {
+      options: {                          // Universal options
+        bundleExec: true,
+        serve: true,
+
+      },
+      serve : {
+        serve: true,
+        server : true,
+        server_port : 4000,
+        auto : true
+      },
+      dev: {
+        src: 'templates',
+        dest: 'dev'
+      },
+      prod: {
+        src: 'templates',
+        dest: 'prod'
+      }
+    },
   });
 
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks("grunt-contrib-jade");
+  grunt.loadNpmTasks("grunt-contrib-connect");
+  grunt.loadNpmTasks('grunt-jekyll');
 
   grunt.registerTask('default', ['jade:publish']);
   grunt.registerTask('publish', ['jade:publish']);
+  grunt.registerTask('test', ['jekyll:serve'])
 
   grunt.registerTask('web', 'Start web server...', function() {
     var options = this.options();
